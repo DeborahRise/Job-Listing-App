ad4c4718369d09fa17e43061b09e2dda1d8c8db7
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-15 12:46:13 +0100

    Updated App.jsx: Implemented new features and fixed styling issues

--- Jobs/src/App.jsx
+++ Jobs/src/App.jsx
@@ -1,98 +1,14 @@
 import React from 'react'
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Homecards from './Components/Homecards';
 
 const App = () => {
   return (
     <>
-    <nav className="bg-indigo-700 border-b border-indigo-500">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
-          <div
-            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-          >
-            {/* <!-- Logo --> */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
-              <img
-                className="h-10 w-auto"
-                src="images/logo.png"
-                alt="React Jobs"
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                >React Jobs</span
-              >
-            </a>
-            <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <a
-                  href="/index.html"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                >
-                <a
-                  href="/jobs.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
-                <a
-                  href="/add-job.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-
-    {/* <!-- Hero --> */}
-    <section className="bg-indigo-700 py-20 mb-4">
-      <div
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-      >
-        <div className="text-center">
-          <h1
-            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-          >
-            Become a React Dev
-          </h1>
-          <p className="my-4 text-xl text-white">
-            Find the React job that fits your skills and needs
-          </p>
-        </div>
-      </div>
-    </section>
-
-    {/* <!-- Developers and Employers --> */}
-    <section className="py-4">
-      <div className="container-xl lg:container m-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Developers</h2>
-            <p className="mt-2 mb-4">
-              Browse our React jobs and start your career today
-            </p>
-            <a
-              href="/jobs.html"
-              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-            >
-              Browse Jobs
-            </a>
-          </div>
-          <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Employers</h2>
-            <p className="mt-2 mb-4">
-              List your job to find the perfect developer for the role
-            </p>
-            <a
-              href="/add-job.html"
-              className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
-            >
-              Add Job
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Navbar />
+    <Hero />
+    <Homecards />
 
     {/* <!-- Browse Jobs --> */}
     <section className="bg-blue-50 px-4 py-10">
