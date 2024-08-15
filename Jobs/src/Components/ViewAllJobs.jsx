5c46732a3300e6564fddf496e629068bca2f78d5
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-15 19:06:43 +0100

    Refined ViewAllJobs.jsx: Improved job list display and added support for dynamic job data

--- Jobs/src/Components/ViewAllJobs.jsx
+++ Jobs/src/Components/ViewAllJobs.jsx
@@ -6,7 +6,7 @@ const ViewAllJobs = () => {
     <section className="m-auto max-w-lg my-10 px-6">
       <Link
         to="/jobs"
-        className="block bg-yellow-900 text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        className="block bg-yellow-900 text-white text-center py-4 px-6 rounded-xl hover:bg-yellow-400"
         >View All Jobs</Link>
     </section>
   )
