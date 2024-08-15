296cc8f2f0667a926baa0e8e2b4959e113e8bab1
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-15 20:39:13 +0100

    Enhanced JobListings.jsx: Integrated axios for job data fetching and improved error handling

--- Jobs/src/Components/JobListings.jsx
+++ Jobs/src/Components/JobListings.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
-import jobs from '../JsonFolder/jobs.json';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import JobList from './JobList';
+import axios from 'axios';
+import { FaSpinner } from 'react-icons/fa';
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome=false}) => {
-    const recentjobs = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect( () => {
+    const fetchJobs = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/jobs');
+        setJobs(response.data);
+      } catch (error) {
+        console.log('Error JobListing', error)
+      } finally {
+        setLoading(false)
+      }
+      };
+    fetchJobs();
+  }, []
+)
+    const recentjobs = isHome ? jobs?.slice(0, 3) : jobs;
   return (
     <section className="bg-yellow-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-yellow-500 mb-6 text-center">
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
+        {loading ? <Spinner loading={loading} /> :
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {recentjobs.map((job) => ( 
+          {recentjobs.map((job) => ( 
                 <JobList key={job.id} job={job}/>
             ))}
+            
         </div>
+        }
       </div>
     </section>
   )
