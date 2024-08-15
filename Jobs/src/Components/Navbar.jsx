de07a7f62c24e3a344a5dc75d33906d6201771c1
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-15 18:19:22 +0100

    Refined Navbar.jsx: Updated navigation links and improved accessibility features

--- Jobs/src/Components/Navbar.jsx
+++ Jobs/src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
           
             {/* <!-- Logo --> */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center mr-4" to="/">
               <img
                 className="h-20 w-auto rounded-full border-2 border-white"
                 src={deblogo}
@@ -20,21 +20,21 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >Jobs</span >
              
-            </a>
+            </Link>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <Link
-                  href="/"
+                  to="/"
                   className="text-black bg-white hover:bg-yellow-900 hover:text-white rounded-md px-3 py-2"
                   >Home</Link>
                 
                 <Link
-                  href="/jobs"
+                  to="/jobs"
                   className="text-white hover:bg-yellow-900 hover:text-white rounded-md px-3 py-2"
                   >Jobs</Link>
                 
                 <Link
-                  href="/add-job"
+                  to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Add Job</Link>
                 
