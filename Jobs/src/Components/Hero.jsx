ead11f7a15d5731120368c49a8b4e4a9ffb7710b
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-15 12:46:13 +0100

    Added Hero.jsx: Implemented the hero section with background image and text overlay

---
+++ Jobs/src/Components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className="bg-yellow-500 py-20 mb-4">
+      <div
+        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+      >
+        <div className="text-center">
+          <h1
+            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+          >
+            Become a Paid Dev
+          </h1>
+          <p className="my-4 text-xl text-white">
+            Find the job that fits your skills and needs
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
