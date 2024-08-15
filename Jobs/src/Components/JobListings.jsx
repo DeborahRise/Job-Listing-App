dd549733c6245880a6359f8c11e263c3373d9c2e
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-15 16:14:24 +0100

    latest changes

---
+++ Jobs/src/Components/JobListings.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import jobs from '../JsonFolder/jobs.json';
+import JobList from './JobList';
+
+const JobListings = () => {
+    const recentjobs = jobs.slice(0, 3);
+  return (
+    <section className="bg-yellow-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-yellow-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {recentjobs.map((job) => ( 
+                <JobList key={job.id} job={job}/>
+            ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default JobListings
