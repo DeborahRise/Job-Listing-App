79304eb783a151d7eedfbd87d09af6bec0e3bceb
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-20 18:56:00 +0100

    Created EditJob.jsx: new component for editing existing job listings with better state management

---
+++ Jobs/src/Pages/EditJob.jsx
@@ -0,0 +1,258 @@
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const EditJob = () => {
+    const [jobs, setJobs] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [type, setType] = useState('Full-Time');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [salary, setSalary] = useState('Under-$50K');
+    const [location, setLocation] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [companyDescription, setCompanyDescription] = useState('');
+    const [contactEmail, setContactEmail] = useState('');
+    const [contactPhone, setContactPhone] = useState('');
+
+    useEffect( () => {
+        const fetchJobs = async () => {
+          try {
+            const response = await axios.get(`/api/jobs/${id}`);
+            const jobData = response.data;
+            setJobs(jobData);
+            setType(jobData.type);
+            setTitle(jobData.title);
+            setDescription(jobData.description);
+            setSalary(jobData.salary);
+            setLocation(jobData.location);
+            setCompanyName(jobData.company.name);
+            setCompanyDescription(jobData.company.description);
+            setContactEmail(jobData.company.contactEmail);
+            setContactPhone(jobData.company.contactPhone)
+            console.log('response.data', response.data);
+          } catch (error) {
+            console.log('Error JobListing', error)
+          } finally {
+            setLoading(false)
+          }
+          };
+        fetchJobs();
+      }, []
+    )
+
+    const EditJobFxn = (event) => {
+        event.preventDefault();
+        
+        const updatedJob = {
+            id,
+            type,
+            title,
+            description,
+            location,
+            salary,
+            company: {
+                name: companyName,
+                description: companyDescription,
+                contactEmail,
+                contactPhone
+            },
+        };
+        updateJobToDB(updatedJob);
+        navigate('/jobs');
+    }
+
+    const updateJobToDB = async (updatedJob) => {
+        const response = await axios.put(`/api/jobs/${id}`, updatedJob, {
+            headers: {'Content-Type': 'Application/JSON'},
+        });
+    }
+
+  return (
+    <section className="bg-yellow-50">
+    <div className="container m-auto max-w-2xl py-24">
+      <div
+        className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0" >
+     
+        <form onSubmit={EditJobFxn}>
+          <h2 className="text-3xl text-center font-semibold mb-6">Update Job</h2>
+
+          <div className="mb-4">
+            <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
+              >Job Type</label>
+            
+            <select
+              id="type"
+              name="type"
+              className="border rounded w-full py-2 px-3"
+              value={ type }
+              onChange={(event) => {setType(event.target.value)}}
+              required>
+            
+              <option value="Full-Time">Full-Time</option>
+              <option value="Part-Time">Part-Time</option>
+              <option value="Remote">Remote</option>
+              <option value="Internship">Internship</option>
+            </select>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2"
+              >Job Listing Name</label >
+           
+            <input
+              type="text"
+              id="title"
+              name="title"
+              className="border rounded w-full py-2 px-3 mb-2"
+              placeholder="eg. Beautiful Apartment In Miami"
+              value={title}
+              onChange={(event) => {setTitle(event.target.value)}}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block text-gray-700 font-bold mb-2"
+              >Description</label>
+            
+            <textarea
+              id="description"
+              name="description"
+              className="border rounded w-full py-2 px-3"
+              rows="4"
+              placeholder="Add any job duties, expectations, requirements, etc"
+              value={ description }
+              onChange={(event) => {setDescription(event.target.value)}}
+            ></textarea>
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
+              >Salary</label>
+            
+            <select
+              id="salary"
+              name="salary"
+              className="border rounded w-full py-2 px-3"
+              value={ salary }
+              onChange={(event) => {setSalary(event.target.value)}}
+              required>
+            
+              <option value="Under $50K">Under $50K</option>
+              <option value="$50K - 60K">$50K - $60K</option>
+              <option value="$60K - 70K">$60K - $70K</option>
+              <option value="$70K - 80K">$70K - $80K</option>
+              <option value="$80K - 90K">$80K - $90K</option>
+              <option value="$90K - 100K">$90K - $100K</option>
+              <option value="$100K - 125K">$100K - $125K</option>
+              <option value="$125K - 150K">$125K - $150K</option>
+              <option value="$150K - 175K">$150K - $175K</option>
+              <option value="$175K - 200K">$175K - $200K</option>
+              <option value="Over $200K">Over $200K</option>
+            </select>
+          </div>
+
+          <div className='mb-4'>
+            <label className='block text-gray-700 font-bold mb-2'>
+              Location
+            </label>
+            <input
+              type='text'
+              id='location'
+              name='location'
+              className='border rounded w-full py-2 px-3 mb-2'
+              placeholder='Company Location'
+              value={ location }
+              onChange={(event) => {setLocation(event.target.value)}}
+              required           
+            />
+          </div>
+
+          <h3 className="text-2xl mb-5">Company Info</h3>
+
+          <div className="mb-4">
+            <label htmlFor="company" className="block text-gray-700 font-bold mb-2"
+              >Company Name</label>
+            
+            <input
+              type="text"
+              id="company"
+              name="company"
+              className="border rounded w-full py-2 px-3"
+              placeholder="Company Name"
+              value={ companyName }
+              onChange={(event) => {setCompanyName(event.target.value)}}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="company_description"
+              className="block text-gray-700 font-bold mb-2"
+              >Company Description</label >
+           
+            <textarea
+              id="company_description"
+              name="company_description"
+              className="border rounded w-full py-2 px-3"
+              rows="4"
+              value={ companyDescription }
+              onChange={(event) => {setCompanyDescription(event.target.value)}}
+              placeholder="What does your company do?"
+            ></textarea>
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="contact_email"
+              className="block text-gray-700 font-bold mb-2"
+              >Contact Email</label>
+            
+            <input
+              type="email"
+              id="contact_email"
+              name="contact_email"
+              className="border rounded w-full py-2 px-3"
+              placeholder="Email address for applicants"
+              value={ contactEmail }
+              onChange={(event) => {setContactEmail(event.target.value)}}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="contact_phone"
+              className="block text-gray-700 font-bold mb-2"
+              >Contact Phone</label >
+           
+            <input
+              type="tel"
+              id="contact_phone"
+              name="contact_phone"
+              className="border rounded w-full py-2 px-3"
+              placeholder="Optional phone for applicants"
+              value={ contactPhone }
+              onChange={(event) => {setContactPhone(event.target.value)}}
+            />
+          </div>
+
+          <div>
+            <button
+              className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+              type="submit">
+            
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default EditJob
