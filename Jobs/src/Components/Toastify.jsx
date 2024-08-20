e8bf6441947879307b49c1eb1c55efd9d303ab48
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-20 19:42:42 +0100

    Added Toastify.jsx: Created a reusable Toastify component to standardize toast notifications across the application, improving user feedback consistency.

---
+++ Jobs/src/Components/Toastify.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const Toastify = () => {
+  return (
+    <ToastContainer />
+  )
+}
+
+export default Toastify
